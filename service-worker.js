d9b41c2dc5420258eac4ccbaedf66868b75ba981
Grugolo <fedego02@outlook.com>
2026-07-31 23:29:34 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = "grugofy-v1";
+
+const FILES = [
+  "/Grugofy/",
+  "/Grugofy/index.html",
+  "/Grugofy/style.css",
+  "/Grugofy/js/main.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
