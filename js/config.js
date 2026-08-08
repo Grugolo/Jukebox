8d2d90b52e0c57daa7db5697ef4414afee38762f
Grugolo <fedego02@outlook.com>
2026-08-08 15:11:48 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // ── config.js ────────────────────────────────────────────────────
-export const YT_API_KEY = 'AIzaSyDRAPLHt6V5eMb4l8fSKobFLy0q47UqUEU';
+export const YT_API_KEY = 'AIzaSyBwZsEchAW5KOytpVE6lqRQaXYdOrsbYT0';
 
